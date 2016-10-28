ed467f0064f5f20721ae674853b45002da6f14f4
Flatiron School <flatironschool@Drew-2.local>
2016-10-28 15:52:48 -0400

    Games controller almost done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 class GamesController < ApplicationController
-  skip_before_action :require_login
 
   def index
     @games = Game.all
@@ -16,18 +15,32 @@ class GamesController < ApplicationController
     if @game.save
       redirect_to @game
     else
+      flash.alert = "You dun fucked up, try again"
       render :new
     end
   end
 
   def show
     @game = Game.find(params[:id])
+    session[:game_id] = @game.id
+    @user = User.find(session[:user_id])
+    @can_edit = @game.gm_id == @user.id
+    # @in_game = if user already has a character in the game
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
+
+    if @game.update(game_params)
+      redirect_to @game
+    else
+      flash.alert = "You dun fucked up, try again"
+      render :new
+    end
   end
 
   def destroy
