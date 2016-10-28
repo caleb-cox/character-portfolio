c48a92bf3ad21e22c4961ec1dbfea4e0e1b2dfde
Flatiron School <flatironschool@Drew-2.local>
2016-10-28 13:39:00 -0400

    First group merge

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:user][:name])
+    @user = User.find_by(name: params[:user][:name])
 
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
-      redirect_to :show, notice: "You are logged in!"
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       flash.alert = "You dun fucked up, try again"
       render 'users#show'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+
   resources :characters, except: [:index]
   resources :games
   resources :character_attributes, only:[:new, :create]
+  resources :users, only: [:show]
   get '/signup' => 'users#new', as: 'signup'
   post '/users' => 'users#create'
   get '/login' => 'sessions#new', as: 'login'
