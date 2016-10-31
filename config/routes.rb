086c4405bb973103a56a8cfe582ac88efabd08e1
calebcox725 <caleb.cox@flatironschool.com>
2016-10-31 09:19:07 -0400

    Write character model tests, write database seed file

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   root to: 'greetings#hello'
   resources :characters, except: [:index]
   resources :games
-  resources :character_attributes, only:[:new, :create]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :create]
+
   get '/signup' => 'users#new', as: 'signup'
-  post '/users' => 'users#create'
   get '/login' => 'sessions#new', as: 'login'
   post '/sessions' => 'sessions#create'
 
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -6,7 +6,41 @@ RSpec.describe Character, type: :model do
 
   describe '#attributes_total' do
     it 'creates a total of commando, hacker and witch attribute points' do
-      expect(character.attributes_total).to eq(10)
+      expect(character.attributes_total).to eq(character.commando + character.hacker + character.witch)
+    end
+  end
+
+  describe '#health' do
+    it 'equals a commando attribute + 6' do
+      expect(character.health).to eq(character.commando + 6)
+    end
+  end
+
+  describe '#luck' do
+    it 'equals a hacker attribute when hacker attribute is > 0' do
+      expect(character.luck).to eq(character.hacker)
+    end
+
+    it 'equals 1 when hacker attribute is 0' do
+      zero_hacker = character
+      zero_hacker.hacker = 0
+      expect(zero_hacker.luck).to eq(1)
+    end
+  end
+
+  describe '#mana' do
+    it 'equals twice the witch attribute' do
+      expect(character.mana).to eq(character.witch * 2)
+    end
+  end
+
+  describe '#skills_must_be_unique' do
+    it 'validates that a character does not have the same skill twice' do
+      not_unique = character
+      not_unique.skills << Skill.find(1)
+      not_unique.skills << Skill.find(1)
+      not_unique.save
+      expect(not_unique.errors[:skills]).to include("may not repeat")
     end
   end
 
