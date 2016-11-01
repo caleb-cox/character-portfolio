336b2ce393d42e9ffd62e02499fd22944e4d323c
Flatiron School <flatironschool@Drew-2.local>
2016-11-01 12:59:55 -0400

    Error Styling on Characters

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -10,16 +10,6 @@ RSpec.describe Game, :type => :model do
         expect(game.save).to eq(true)
       end
 
+      
 
-end
-
-
-RSpec.describe GamesController, :type => :controller do
-  describe "GET #show" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :index
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-  end
 end
